a2706dca3088f8a66be4600711f3f3456961ec49
4lan <4lan@live.com>
2018-03-31 19:35:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,142 @@
+package com.example.android.courtcounter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+    int foulTeamA = 0;
+    int foulTeamB = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+/**
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+*/
+    /**
+     * Increase the score for Team A by 1 point.
+     */
+    /**
+    public void addOneForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+     */
+
+    /**
+     * Increase the foul for Team A by 1 point.
+     */
+    public void foulForTeamA(View v) {
+        foulTeamA = foulTeamA + 1;
+        displayFoulForTeamA(foulTeamA);
+    }
+
+    /**
+     * Increase the score for Team A by 1 point.
+     */
+    public void goalForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given foul for Team A.
+     */
+    public void displayFoulForTeamA(int foul) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_foul);
+        scoreView.setText(String.valueOf(foul));
+    }
+
+    /**
+     * Increase the score for Team B by 1 point.
+     */
+    public void goalForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the foul for Team B by 1 point.
+     */
+    public void foulForTeamB(View v) {
+        foulTeamB = foulTeamB + 1;
+        displayFoulForTeamB(foulTeamB);
+    }
+
+    /**
+     * Increase the score for Team A by 3 points.
+     */
+    /**
+    public void addThreeForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+     */
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given foul for Team B.
+     */
+    public void displayFoulForTeamB(int foul) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_foul);
+        scoreView.setText(String.valueOf(foul));
+    }
+
+    /**
+     *
+     */
+    public void resetAll(View v) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        foulTeamA = 0;
+        foulTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+        displayFoulForTeamA(foulTeamA);
+        displayFoulForTeamB(foulTeamB);
+    }
+}
